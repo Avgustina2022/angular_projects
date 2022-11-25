22ca931d4054c24ef703266a47475ea205e14aa7
IRINA <108739078+Avgustina2022@users.noreply.github.com>
2022-11-25 09:13:32 +0300

    лого и readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+
 import { ProductComponent } from './components/pages/home/product/product.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product-service';
 import { CartComponent} from './components/pages/cart/cart.component';
 import { CartService } from './services/cart-service';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
